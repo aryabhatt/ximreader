d127a18bcbfadb1cdcaf3fc0f6a941202f620955
Dinesh Kumar <dkumar@lbl.gov>
2024-04-10 12:05:00 -0700

    added docstring

--- ximreader/ximReader.py
+++ ximreader/ximReader.py
@@ -2,9 +2,17 @@ import numpy as np
 from .libxim import ximreader
 
 def ximReader(ximfile):
+    """ Read xim image file.
+
+    Parameters:
+    ximfile (str) -- full path to xim image file
+
+
+    Returns:
+        numpy.ndarray: Image data
+    """
     header, image = ximreader(ximfile)
     nrows = header['width']
     ncols = header['height']
-    return header, image.reshape(nrows, ncols)
+    return image.reshape(nrows, ncols)
 
-    
